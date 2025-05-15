c725433cec0bc8a14e81ce34b90dab43999b2a75
michael1303 <88162384+michael1303@users.noreply.github.com>
2025-05-15 10:59:50 -0400

    add LinkedList function skeletons

--- LinkedList.js
+++ LinkedList.js
@@ -8,7 +8,53 @@ export default class LinkedList{
         this.size = 0;
     }
 
+    // Adds new node (with value) to end of list
+    append(value){
 
+    }
 
+    //Adds new node (with value) to start of list
+    prepend(value){
 
+    }
+
+    //Returns size (# of nodes)
+    size(){
+
+    }
+
+    //Returns first node
+    head(){
+
+    }
+
+    //Returns last node 
+    tail(){
+
+    }
+
+    //Returns node at given index
+    at(index){
+
+    }
+
+    //Removes last element from list 
+    pop(){
+
+    }
+
+    //Returns true if passed value is in list, false otherwise
+    contains(value){
+
+    }
+
+    //Returns index of node containing value, or null if not found
+    find(value){
+
+    }
+
+    //Represents LinkedList objects as strings (value) -> (value) -. (value) -> null
+    toString(){
+        
+    }
 }
